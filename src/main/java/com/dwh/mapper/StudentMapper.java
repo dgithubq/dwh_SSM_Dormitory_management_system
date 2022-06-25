a87b8a9e7f3e0a7fcbff48475433f461a823cceb
dgithubq <dwhguge@gmail.com>
2022-06-25 14:26:27 +0800

    新增了一个登记学生迁出版块中设置学生状态为迁出的操作

--- src/main/java/com/dwh/mapper/StudentMapper.java
+++ src/main/java/com/dwh/mapper/StudentMapper.java
@@ -45,6 +45,11 @@ public interface StudentMapper {
     @Select("update student set dormitory_id = #{param2} where id = #{param1}")
     public void resetDormitoryId(Integer studentId, Integer availableDormitoryId);
 
+    /**
+     * 以下是迁出板块内容，但是显示的学生信息，故而写在studentMapper
+     * @return
+     */
+
     //查询住宿状态是入住的学生信息（为了给迁出学生那一版块显示信息）
     @Select("select s.id,s.number,s.name name,s.gender,s.dormitory_id dormitory_id,d.name dormitoryName,s.state from student s,dormitory d where s.dormitory_id = d.id and s.state = '入住'")
     public List<Student> moveOutList();
